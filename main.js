6afe18b8f83043cc081af28c2e636e2ebccea384
Kwon <sjkwon6653@gmail.com>
2020-04-04 19:23:25 +0900

    append

---
+++ main.js
@@ -0,0 +1,56 @@
+var shortid = require('shortid')
+var low = require('lowdb')
+var FileSync = require('lowdb/adapters/FileSync')
+var adapter = new FileSync('db.json')
+var db = low(adapter)
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ 
+	topic: [],
+	author: []
+}).write();
+
+// db.get('author').push({
+// 	id:1,
+// 	name:'egoing',
+// 	profile:'developrt'
+// }).write();
+// db.get('topic').push({
+// 	id:1,
+// 	title:'lowdb',
+// 	description:'lowdb is ....',
+// 	author:1
+// }).write();
+// db.get('topic').push({
+// 	id:2,
+// 	title:'mysql',
+// 	description:'mysql is ....',
+// 	author:1
+// }).write();
+// console.log(
+// 	db.get('topic')
+// 	.find({title:'lowdb', author:1})
+// 	.value()
+// );
+
+// db.get('topic')
+// .find({id:2})
+// .assign({title:'MYSQL & MariaDB'})
+// .write();
+
+var sid = shortid.generate();
+db.get('topic')
+	.push({
+		id:sid,
+		name:'taeho',
+		profile:'data scientist'
+	}).write();
+
+db.get('author')
+	.push({
+		id:shortid.generate(),
+		title:'PostgreSQL',
+		description:'PostgreSQL is ....',
+		author:sid
+	}).write();
+
